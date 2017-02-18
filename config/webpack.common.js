97bc03d3180550d8302bf7e7cd695938ee69bd17
sazonenka <aliaksandr.sazonenka@gmail.com>
2017-02-17 17:05:27 -0800

    Update baseUrl

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -28,7 +28,7 @@ const HMR = helpers.hasProcessFlag('hot');
 const AOT = helpers.hasNpmFlag('aot');
 const METADATA = {
   title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
-  baseUrl: '/',
+  baseUrl: '/angular2-webpack-starter',
   isDevServer: helpers.isWebpackDevServer()
 };
 
